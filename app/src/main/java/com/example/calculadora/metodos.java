64f7f044b015e3d2069872e51fd6d1815e89624d
Victor Guzmán <80615275+GuzmanVic@users.noreply.github.com>
2023-09-19 13:08:12 -0600

    cc2

--- app/src/main/java/com/example/calculadora/metodos.java
+++ app/src/main/java/com/example/calculadora/metodos.java
@@ -9,14 +9,14 @@ import java.util.Stack;
 
 public class metodos {
 
-    public void numerales(Button[] numeros, TextView display) {//Escribe los números presionados por el usuario en e display
+    public void numerales(Button[] numeros, TextView display) {//Captures on the display the numbers pressed by the user
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i].setTag(i); // Asigna el número como una etiqueta al botón
+            numeros[i].setTag(i); //Assigns the number as a tag to the button
             numeros[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Integer numeroPresionado = (Integer) view.getTag(); // Obtiene el número de la etiqueta
-                    display.append(numeroPresionado.toString()); // Agrega el número al TextView
+                    Integer numeroPresionado = (Integer) view.getTag(); // Obtains the number from the tag
+                    display.append(numeroPresionado.toString()); // Add the number to the TextView
                 }
             });
         }
@@ -29,6 +29,8 @@ public class metodos {
             if (Character.isDigit(last) || last == ')') { // Check if the last character is a digit
                 return op;
             }
+        } else if (op.equals("√")) {
+            return "2" + op;
         }
         return ""; // Return the 'allow' variable indicating whether it's allowed to write an exponent
     }
@@ -75,48 +77,57 @@ public class metodos {
     }
 
     public static double resolver(String expression) {
-        Stack<Double> operandStack = new Stack<>();  // Stack for the operands
-        Stack<Character> operatorStack = new Stack<>();  // Stack for the operators
+        // Stack para almacenar los operandos (números)
+        Stack<Double> operandStack = new Stack<>();
+        // Stack para almacenar los operadores
+        Stack<Character> operatorStack = new Stack<>();
 
+        // Recorre la expresión matemática caracter por caracter
         for (int i = 0; i < expression.length(); i++) {
-            char c = expression.charAt(i);
+            char c = expression.charAt(i); // Almacena cada uno de los caracteres a lo largo del ciclo
 
-            if (Character.isDigit(c) || c == '.') {  // If the character is a digit or a decimal point
+            if (Character.isDigit(c) || c == '.') {  // Si el carácter es un dígito o un punto decimal
                 StringBuilder operand = new StringBuilder();
+                // Mientras el siguiente carácter sea un dígito o un punto decimal, construye el operando
                 while (i < expression.length() && (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.')) {
-                    operand.append(expression.charAt(i));  // Build an operand
+                    operand.append(expression.charAt(i));  // Construye el operando
                     i++;
                 }
-                operandStack.push(Double.parseDouble(operand.toString()));  // Convierte el operando a un número y lo agrega a la pila
+                // Convierte el operando a un número y lo agrega a la pila de operandos
+                operandStack.push(Double.parseDouble(operand.toString()));
                 i--;  // Retrocede un paso para compensar el avance en el bucle while
             } else if (c == '(') {  // Si el carácter es un paréntesis de apertura
                 operatorStack.push(c);  // Agrega el paréntesis de apertura a la pila de operadores
             } else if (c == ')') {  // Si el carácter es un paréntesis de cierre
                 while (!operatorStack.isEmpty() && operatorStack.peek() != '(') {
-                    applyOperator(operandStack, operatorStack);  // Aplica operadores hasta encontrar el paréntesis de apertura correspondiente
+                    // Aplica operadores hasta encontrar el paréntesis de apertura correspondiente
+                    applyOperator(operandStack, operatorStack);
                 }
                 if (!operatorStack.isEmpty() && operatorStack.peek() == '(') {
                     operatorStack.pop();  // Retira el paréntesis de apertura de la pila de operadores
                 }
             } else if (isOperator(c)) {  // Si el carácter es un operador
                 while (!operatorStack.isEmpty() && precedence(operatorStack.peek()) >= precedence(c)) {
-                    applyOperator(operandStack, operatorStack);  // Aplica operadores de acuerdo a la jerarquía de operaciones
+                    // Aplica operadores de acuerdo a la jerarquía de operaciones
+                    applyOperator(operandStack, operatorStack);
                 }
                 operatorStack.push(c);  // Agrega el operador a la pila de operadores
             }
         }
 
+        // Aplica operadores restantes
         while (!operatorStack.isEmpty()) {
-            applyOperator(operandStack, operatorStack);  // Aplica operadores restantes
+            applyOperator(operandStack, operatorStack);
         }
 
         if (operandStack.isEmpty()) {
             throw new IllegalArgumentException("La expresión no es válida");  // Lanza una excepción si no quedan operandos
         }
 
-        return operandStack.pop();  // Devuelve el resultado final
-
+        // Devuelve el resultado final
+        return operandStack.pop();
     }
+
 
     private static boolean isOperator(char c) {
         return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '√' || c == '%';
@@ -134,49 +145,70 @@ public class metodos {
     }
 
     private static void applyOperator(Stack<Double> operandStack, Stack<Character> operatorStack) {
+        // Comprueba que haya al menos dos operandos en la pila de operandos y que no esté vacía
         if (operandStack.size() < 2 || operatorStack.isEmpty()) {
             throw new IllegalArgumentException("La expresión no es válida");
+        } else if (operandStack.size() == 1 && operatorStack.contains("√")) {
+            // En caso de tener un solo operando y el operador es '√' (raíz cuadrada)
+            double operand1 = operandStack.pop(); // Obtiene el único operando
+
+            // Realiza la operación de raíz cuadrada
+            if (operand1 < 0) {
+                throw new ArithmeticException("Raíz cuadrada de un número negativo");
+            }
+            double result = Math.sqrt(operand1);
+
+            // Agrega el resultado a la pila de operandos
+            operandStack.push(result);
+
+            // Elimina el operador de raíz cuadrada de la pila de operadores
+            operatorStack.remove("√");
         }
 
+        // Obtiene los dos operandos superiores de la pila de operandos
         double operand2 = operandStack.pop();
         double operand1 = operandStack.pop();
+
+        // Obtiene el operador superior de la pila de operadores
         char operator = operatorStack.pop();
 
-        double result = 0.0;
+        double result = 0.0; // Inicializa la variable de resultado con 0.0
 
+        // Utiliza un interruptor (switch) para realizar la operación correspondiente
         switch (operator) {
             case '+':
-                result = operand1 + operand2;
+                result = operand1 + operand2; // Suma los operandos
                 break;
             case '-':
-                result = operand1 - operand2;
+                result = operand1 - operand2; // Resta los operandos
                 break;
             case '*':
-                result = operand1 * operand2;
+                result = operand1 * operand2; // Multiplica los operandos
                 break;
             case '/':
                 if (operand2 == 0) {
                     throw new ArithmeticException("División por cero");
                 }
-                result = operand1 / operand2;
+                result = operand1 / operand2; // Divide los operandos
                 break;
             case '%':
                 if (operand2 == 0) {
                     throw new ArithmeticException("Módulo por cero");
                 }
-                result = operand1 % operand2;
+                result = operand1 % operand2; // Calcula el módulo de los operandos
                 break;
             case '^':
-                result = Math.pow(operand1, operand2);
+                result = Math.pow(operand1, operand2); // Calcula la potencia de operand1 elevado a operand2
                 break;
             case '√':
                 if (operand1 < 0) {
                     throw new ArithmeticException("Raíz cuadrada de un número negativo");
                 }
-                result = Math.sqrt(operand1);
+                result = Math.sqrt(operand2); // Calcula la raíz cuadrada de operand1
                 break;
         }
 
+        // Agrega el resultado de la operación a la pila de operandos
         operandStack.push(result);
     }
 
